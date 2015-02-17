b6dab378cf883edbac0485ff0145de1a0540ac7e
veter069 <wind069@gmail.com>
2015-02-17 09:29:17 +0500

    Telerik

--- PCsoft/SerialPortUsing/AdminForm.Designer.cs
+++ PCsoft/SerialPortUsing/AdminForm.Designer.cs
@@ -35,6 +35,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.насстройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.systemUsersBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aC_DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.staffBindingSource1)).BeginInit();
@@ -99,11 +100,21 @@
             this.насстройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
             this.насстройкиToolStripMenuItem.Text = "Настройки";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(813, 414);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 453);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -140,5 +151,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem насстройкиToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
 	}
 }
