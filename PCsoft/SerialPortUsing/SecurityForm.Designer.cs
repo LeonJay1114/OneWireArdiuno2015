2db9cf8aaf825f5448eb6b7a8376e42b2387f57d
FROSTEROID <frosteroidous@gmail.com>
2015-02-22 16:20:39 +0500

    SecurityForm
    Тасуем контролы

--- PCsoft/SerialPortUsing/SecurityForm.Designer.cs
+++ PCsoft/SerialPortUsing/SecurityForm.Designer.cs
@@ -32,9 +32,9 @@
 			// 
 			// pic_face
 			// 
-			this.pic_face.Location = new System.Drawing.Point(369, 12);
+			this.pic_face.Location = new System.Drawing.Point(353, 12);
 			this.pic_face.Name = "pic_face";
-			this.pic_face.Size = new System.Drawing.Size(584, 425);
+			this.pic_face.Size = new System.Drawing.Size(600, 800);
 			this.pic_face.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pic_face.TabIndex = 0;
 			this.pic_face.TabStop = false;
@@ -42,7 +42,7 @@
 			// l1
 			// 
 			this.l1.AutoSize = true;
-			this.l1.Location = new System.Drawing.Point(366, 440);
+			this.l1.Location = new System.Drawing.Point(303, 816);
 			this.l1.Name = "l1";
 			this.l1.Size = new System.Drawing.Size(34, 13);
 			this.l1.TabIndex = 1;
@@ -51,15 +51,15 @@
 			// l_fio
 			// 
 			this.l_fio.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.l_fio.Location = new System.Drawing.Point(406, 440);
+			this.l_fio.Location = new System.Drawing.Point(343, 816);
 			this.l_fio.Name = "l_fio";
-			this.l_fio.Size = new System.Drawing.Size(547, 106);
+			this.l_fio.Size = new System.Drawing.Size(610, 100);
 			this.l_fio.TabIndex = 2;
 			this.l_fio.Text = "ФИО";
 			// 
 			// cB_testButton
 			// 
-			this.cB_testButton.Location = new System.Drawing.Point(878, 605);
+			this.cB_testButton.Location = new System.Drawing.Point(12, 893);
 			this.cB_testButton.Name = "cB_testButton";
 			this.cB_testButton.Size = new System.Drawing.Size(75, 23);
 			this.cB_testButton.TabIndex = 3;
@@ -71,7 +71,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(965, 640);
+			this.ClientSize = new System.Drawing.Size(965, 925);
 			this.Controls.Add(this.cB_testButton);
 			this.Controls.Add(this.l_fio);
 			this.Controls.Add(this.l1);
