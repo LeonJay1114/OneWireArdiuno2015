bb119b509df769998938f82d50a29ea1348bc461
root <root@localhost.localdomain>
2015-03-10 11:40:11 +0000

    Now arduino gives out 9(NINE) bytes. The last byte is signalling if the UID was given from exit or enter connectors. That will allow us to know whether we must write an enter or exit event to our log.

--- arduinoSketches/OneWireRetranslator/OneWireRetranslator.ino
+++ arduinoSketches/OneWireRetranslator/OneWireRetranslator.ino
@@ -1,12 +1,15 @@
 #include <OneWire.h>// Библиотека, позволяющая пользоваться протоколом OneWire, не зная протокола OneWire
 
-#define touchPIN 10
+#define EnterPIN 10
+#define ExitPIN 11
 
-const int BUZZER_PINUMBER = 8; // номер пина управления пищалкой
-const int NEW_KEY_CHECK_DELAY = 1500; // задержка между проверками поднесённости карты в миллисекундах
+//const int BUZZER_PINUMBER = 8; // номер пина управления пищалкой
+const int NEW_KEY_CHECK_DELAY = 750; // задержка между проверками поднесённости карты в миллисекундах
 
-OneWire ds(touchPIN); // использующийся пин отправляется инициализатору класса OneWire
-byte curKey[8];
+OneWire dsEnter(EnterPIN); // использующийся пин отправляется инициализатору класса OneWire
+OneWire dsExit(ExitPIN);
+byte curKey[9];
+
 
 void setup(){
 	Serial.begin(9600); // Начать общение с компом по последовательному порту
@@ -14,28 +17,48 @@ void setup(){
 
 void loop(){
 	delay(NEW_KEY_CHECK_DELAY);
+	if(SearchEnterKey()){
+		PrintKey();
+	}
 
-	if(SearchKey()){
+	delay(NEW_KEY_CHECK_DELAY);
+	if(SearchExitKey()){
 		PrintKey();
 	}
 	
 }
 
-bool SearchKey(){
-	ds.reset();
+bool SearchEnterKey(){
+	dsEnter.reset();
 	delay(50);
-	ds.write(0x33); // "READ" command
+	dsEnter.write(0x33); // "READ" command
 
-	ds.read_bytes(curKey, 8);
+	dsEnter.read_bytes(curKey, 8);
 
 	if (curKey[0] & curKey[1] & curKey[2] & curKey[3] & curKey[4] & curKey[5] & curKey[6] & curKey[7] == 0xFF){
 		return false;
 	}
+	curKey[8] = 0;
 	return true;
 }
 
+bool SearchExitKey(){
+	dsExit.reset();
+	delay(50);
+	dsExit.write(0x33); // "READ" command
+
+	dsExit.read_bytes(curKey, 8);
+
+	if (curKey[0] & curKey[1] & curKey[2] & curKey[3] & curKey[4] & curKey[5] & curKey[6] & curKey[7] == 0xFF){
+		return false;
+	}
+	curKey[8] = 1;
+	return true;
+}
+
+
 void PrintKey(){
-	for(byte i = 0; i < 8; i++) {
+	for(byte i = 0; i < 9; i++) {
 		Serial.print((char)curKey[i]);
 	}
 }
