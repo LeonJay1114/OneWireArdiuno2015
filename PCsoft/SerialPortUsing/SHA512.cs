7039bc4cf0e28412137ff7ff5c5c65fac4d8d7f0
FROSTEROID <frosteroidous@gmail.com>
2015-03-13 01:56:33 +0500

    Added a file for a new SHA512 class

---
+++ PCsoft/SerialPortUsing/SHA512.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+/// <summary>
+/// Summary description for sha256
+/// </summary>
+public class SHA512 {
+	public SHA512() {
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+	public static string EncodePassword(string originalPassword) {
+		var sha = System.Security.Cryptography.SHA512Managed.Create();
+
+		//originalBytes = ASCIIEncoding.Default.GetBytes(originalPassword);
+		//encodedBytes = sha512.ComputeHash(originalBytes);
+		//Convert encoded bytes back to a 'readable' string
+		//return BitConverter.ToString(encodedBytes);
+		return null;
+	}
+}
