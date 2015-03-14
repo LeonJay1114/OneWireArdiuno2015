41967716bd861f51fc2856f845e5a4cf089e21d0
FROSTEROID <frosteroidous@gmail.com>
2015-03-14 05:28:57 +0500

    SHA512 using class is ready now.
    Now we must rebuid our database to use hashes and salts.

--- PCsoft/SerialPortUsing/SHA512.cs
+++ PCsoft/SerialPortUsing/SHA512.cs
@@ -1,25 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Security.Cryptography;
 using System.Text;
-/// <summary>
-/// Summary description for sha256
-/// </summary>
-public class SHA512 {
-	public SHA512() {
-		//
-		// TODO: Add constructor logic here
-		//
-	}
-	public static string EncodePassword(string originalPassword) {
-		var sha = System.Security.Cryptography.SHA512Managed.Create();
 
-		//originalBytes = ASCIIEncoding.Default.GetBytes(originalPassword);
-		//encodedBytes = sha512.ComputeHash(originalBytes);
-		//Convert encoded bytes back to a 'readable' string
-		//return BitConverter.ToString(encodedBytes);
-		return null;
+namespace SerialPortUsing {
+	public static class SHA512 {
+		public static string EncodePassword(string password, int saltLength, ref string salt) {
+			byte[] passBytes = Encoding.Unicode.GetBytes(password);
+			byte[] saltBytes = MakeSalt(saltLength);
+
+			int totalL = passBytes.Length + saltBytes.Length;
+			byte[] passAndSalt = new byte[totalL];
+
+			passBytes.CopyTo(passAndSalt, 0);
+			saltBytes.CopyTo(passAndSalt, passBytes.Length);
+
+			SHA512Managed sha = new SHA512Managed();
+			byte[] hashed = sha.ComputeHash(passAndSalt, 0, totalL);
+			//byte[] hashed = sha.TransformFinalBlock(passAndSalt, 0, totalL); Этот считает блоками по 1 байту. Не камильфо.
+
+			salt = Convert.ToBase64String(saltBytes);
+			return Convert.ToBase64String(hashed);
+		}
+
+		public static bool CheckPassword(string password, string salt, string hash) {
+			byte[] passBytes = Encoding.Unicode.GetBytes(password);
+			byte[] saltBytes = Convert.FromBase64String(salt);
+
+			int totalL = passBytes.Length + saltBytes.Length;
+			byte[] passAndSalt = new byte[totalL];
+
+			passBytes.CopyTo(passAndSalt, 0);
+			saltBytes.CopyTo(passAndSalt, passBytes.Length);
+
+			SHA512Managed sha = new SHA512Managed();
+			byte[] hashed = sha.ComputeHash(passAndSalt, 0, totalL);
+
+			return Convert.ToBase64String(hashed) == hash;
+		}
+
+		private static byte[] MakeSalt(int saltLength) {
+			byte[] rez = new byte[saltLength];
+			Random r = new Random();
+			for (int i = 0; i < saltLength; i++) {
+				rez[i] = (byte)r.Next(255);
+			}
+			return rez;
+		}
+
 	}
 }
